d9a40f42518be1924aa6e2c11ba9b0a05283344a
kiedunne <kie.dunne@gmail.com>
2019-02-25 12:06:35 +0000

    add submit functions

---
+++ src/TodoList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+
+  render(){
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.props.addItem} >
+            <input
+            placeholder="Task"
+            ref={this.props.inputElement}
+            value={this.props.currentItem.text}
+            onChange={this.props.handleInput}
+            />
+            <button className="btn btn-info btn-sm m-2" type="submit">Add Task</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default TodoList
