6efd5a832694fa1cac8e8305abd2154902b95519
kiedunne <kie.dunne@gmail.com>
2019-02-25 16:19:00 +0000

    updated note style :lipstick:

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -4,17 +4,20 @@ class TodoItems extends Component {
 
     createTasks = item => {
         return (
-        <li key={item.key} onClick={() => this.props.deleteItem(item.key) }>
-        {item.text}
-        </li>
-        )
+        <div className='eachNote'>
+            <div key={item.key}>{item.text}</div>
+            <button onClick={() => this.props.deleteItem(item.key) } className="btn btn-danger btn-sm m-2" type="submit">Delete</button>
+        </div>)
     }
     render() {
         const todoEntries = this.props.entries 
         const listItems = todoEntries.map(this.createTasks)
 
-        return <ul className="theList">{listItems}</ul>
+        return <div
+            className="theList">{listItems} 
+        </div>
     }
+
 }
 
 export default TodoItems
